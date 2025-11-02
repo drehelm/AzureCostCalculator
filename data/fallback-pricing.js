884c2b06dd558fa66a17b210d7ff92f59d233a2a
GitHub Action <action@github.com>
2025-11-02 01:43:21 +0000

    Update Azure pricing data (automated)

--- data/fallback-pricing.js
+++ data/fallback-pricing.js
@@ -9,11 +9,11 @@
  * current Azure pricing. For the most accurate pricing,
  * the application will attempt to use the Azure Retail Prices API.
  * 
- * Last updated: October 26, 2025
+ * Last updated: November 2, 2025
  */
 
 const FALLBACK_PRICING_DATA = {
-  "lastUpdated": "2025-10-26T01:41:14.981Z",
+  "lastUpdated": "2025-11-02T01:43:17.608Z",
   "virtualMachines": {
     "eastus": {
       "dsv3": {
